850af219d1592988621c21b441e345a5cf7638f8
phoenix marketing <phoenixmarketing21@gmail.com>
2025-11-19 15:42:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5896997028-cf2e8",
+  "appId": "1:741960765246:web:bb8395149d53fd716ca66f",
+  "apiKey": "AIzaSyAEe8PJx3YNgfSS5XrqZoyNplUABTxNi1s",
+  "authDomain": "studio-5896997028-cf2e8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "741960765246"
+};
